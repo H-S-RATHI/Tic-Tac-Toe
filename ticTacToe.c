d084a9ea58e67b8e53d41994c254cfb5b29188f1
H-S-RATHI <goluu.000111@gmail.com>
2023-09-30 09:56:29 +0530

    Updated Virsion

--- ticTacToe.c
+++ ticTacToe.c
@@ -1,86 +1,81 @@
-#include<stdio.h>
-#include <conio.h>
-#include <windows.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-char square[10] = {'0','1','2','3','4','5','6','7','8','9'};
-int checkWin();
-void drawBoard();
+void drawBoard(char square[]);
+int checkWin(char square[]);
+int isBoardFull(char square[]);
 
-int main(){
-    int player = 1,i,choice;
-    char mark;
+int main() {
+    char square[10] = {'0','1','2','3','4','5','6','7','8','9'};
+    int player = 1, choice, result;
 
-    do{
-        drawBoard();
-        player = (player%2)?1:2;
-        printf("\t\tPlayer %d enter the choice :",player);
-        scanf("%d",&choice);
-        mark = (player == 1)?'X' : 'O';
+    do {
+        drawBoard(square);
+        player = (player % 2) ? 1 : 2;
+        printf("\t\tPlayer %d, enter a number: ", player);
+        scanf("%d", &choice);
 
-        if (choice == 1 && square[1] == '1'){
-            square[1] = mark;
-        }else if(choice == 2 && square[2] == '2'){
-            square[2] = mark;
-        }else if(choice == 3 && square[3] == '3'){
-            square[3] = mark;
-        }else if(choice == 4 && square[4] == '4'){
-            square[4] = mark;
-        }else if(choice == 5 && square[5] == '5'){
-            square[5] = mark;
-        }else if(choice == 6 && square[6] == '6'){
-            square[6] = mark;
-        }else if(choice == 7 && square[7] == '7'){
-            square[7] = mark;
-        }else if(choice == 8 && square[8] == '8'){
-            square[8] = mark;
-        }else if(choice == 9 && square[9] == '9'){
-            square[9] = mark;
-        }else {
-            printf("Invalid option !");
-            player--;
-            getch();
+        char mark = (player == 1) ? 'X' : 'O';
+
+        if (choice >= 1 && choice <= 9 && square[choice] == '0' + choice) {
+            square[choice] = mark;
+            result = checkWin(square);
+
+            if (result == 1) {
+                drawBoard(square);
+                printf("\t\t==>Player %d won!\n", player);
+                break;
+            } else if (result == 0) {
+                drawBoard(square);
+                printf("\t\t==>Game draw!\n");
+                break;
+            }
+        } else {
+            printf("Invalid option! Try again.\n");
+            continue;
         }
-        i = checkWin();
-        player++;  
-    }while(i == -1);
-    
-    if (i==1){
-         printf("\t\t==>Player %d won", --player);
-    }else{
-        printf("\t\t==>Game draw");
-    }
-    getch();
+
+        player++;
+    } while (1);
+
     return 0;
 }
-int checkWin(){
-    if (square[1] == square[2] && square[2] == square[3]){
-        return 1;
-    }else if(square[4] == square[5] && square[5] == square[6]){
-        return 1;
-    }else if(square[7] == square[8] && square[8] == square[9]){
-        return 1;
-    }else if(square[1] == square[4] && square[4] == square[7]){
-        return 1;
-    }else if(square[2] == square[5] && square[5] == square[8]){
-        return 1;
-    }else if(square[3] == square[6] && square[6] == square[9]){
-        return 1;
-    }else if(square[3] == square[5] && square[5] == square[7]){
-        return 1;
-    }else if(square[1] == square[5] && square[5] == square[9]){
-        return 1;
-    }else if(square[1] != '1' && square[2] != '2' && square[3] != '3' && square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] != '7' && square[8] != '8' && square[9] != '9'){
-        return 0;
-    }else{
-    return -1;
+
+int checkWin(char square[]) {
+    int winPatterns[8][3] = {
+        {1, 2, 3}, {4, 5, 6}, {7, 8, 9},
+        {1, 4, 7}, {2, 5, 8}, {3, 6, 9},
+        {1, 5, 9}, {3, 5, 7}
+    };
+
+    for (int i = 0; i < 8; i++) {
+        int a = winPatterns[i][0];
+        int b = winPatterns[i][1];
+        int c = winPatterns[i][2];
+
+        if (square[a] == square[b] && square[b] == square[c])
+            return 1;
     }
+
+    if (isBoardFull(square))
+        return 0;
+
+    return -1;
 }
 
-void drawBoard(){
-    system("cls");
-    printf("\n\n\t\t-------Tic Tac Toe------- \n\n");
-    printf("\t\tPlayer1 (X) - Player2 (0) \n\n\n");
+int isBoardFull(char square[]) {
+    for (int i = 1; i <= 9; i++) {
+        if (square[i] != 'X' && square[i] != 'O')
+            return 0;
+    }
+    return 1;
+}
+
+void drawBoard(char square[]) {
+    system("clear"); // Use "clear" for Unix-based systems, "cls" for Windows
+
+    printf("\n\n\t\t-------Tic Tac Toe-------\n\n");
+    printf("\t\tPlayer1 (X) - Player2 (O)\n\n\n");
     printf("\t\t ________ _______ _______\n\n");
     printf("\t\t|        |       |       |\n");
     printf("\t\t|   %c    |  %c    |   %c   |\n", square[1], square[2], square[3]);
